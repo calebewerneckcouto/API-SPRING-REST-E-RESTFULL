f5d1d7e310f99df2900e0c2040900c50d95314af
calebewerneckcouto <calebewerneck@hotmai.com>
2020-02-15 15:46:30 -0300

    API REST E RESTFULL COM SPRING

---
+++ src/main/java/curso/api/rest/repositoy/UsuarioRepository.java
@@ -0,0 +1,26 @@
+package curso.api.rest.repositoy;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import curso.api.rest.model.Usuario;
+
+@Repository
+public interface UsuarioRepository  extends CrudRepository<Usuario, Long>{
+	
+	@Query("select u from Usuario u where u.login = ?1")
+	Usuario findUserByLogin(String login);
+	
+	
+	
+	@Query("select u from Usuario u where u.nome like %?1%")
+	List<Usuario> findUserByNome(String nome);
+	
+	
+	
+
+
+}
